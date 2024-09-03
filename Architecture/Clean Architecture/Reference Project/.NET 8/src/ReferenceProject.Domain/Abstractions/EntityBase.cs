a5ab157c7760f6c98ff9c7456035c56198c07b2d
avf2001 <avf2001@yandex.ru>
2024-09-03 22:57:10 +0300

    В проект ReferenceProject.Domain добавлен новый класс EntityBase

---
+++ Architecture/Clean Architecture/Reference Project/.NET 8/src/ReferenceProject.Domain/Abstractions/EntityBase.cs
@@ -0,0 +1,41 @@
+﻿namespace ReferenceProject.Domain.Abstractions
+{
+    public abstract class EntityBase
+    {
+        #region Свойства
+
+        public int Id { get; init; }
+
+        #endregion
+
+        #region Конструкторы
+
+        // For EntityFramework
+        protected EntityBase() { }
+
+        protected EntityBase(int id) => Id = id;
+
+        #endregion
+
+        #region Domain Events
+
+        private readonly List<IDomainEvent> _domainEvents = [];
+
+        public IReadOnlyList<IDomainEvent> GetDomainEvents()
+        {
+            return _domainEvents.ToList();
+        }
+
+        public void ClearDomainEvents()
+        {
+            _domainEvents.Clear();
+        }
+
+        protected void RaiseDomainEvent(IDomainEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
+        }
+
+        #endregion        
+    }
+}
