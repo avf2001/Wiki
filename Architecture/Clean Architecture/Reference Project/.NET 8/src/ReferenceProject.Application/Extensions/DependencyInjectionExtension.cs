da36fa3485bc9c1f96cd6f2608d83392723db1e1
avf2001 <avf2001@yandex.ru>
2024-09-24 09:35:51 +0300

    Update DependencyInjectionExtension.cs

--- Architecture/Clean Architecture/Reference Project/.NET 8/src/ReferenceProject.Application/Extensions/DependencyInjectionExtension.cs
+++ Architecture/Clean Architecture/Reference Project/.NET 8/src/ReferenceProject.Application/Extensions/DependencyInjectionExtension.cs
@@ -1,1 +1,28 @@
+using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+//using AutoMapperProfile class
+
+namespace ReferenceProject.Application.Extensions
+{
+    public static class DependencyInjectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            #region MediatR
+
+            services.AddMediatR(System.Reflection.Assembly.GetExecutingAssembly());
+
+            #endregion
+
+            #region AutoMapper
+
+            services.AddAutoMapper(typeof(AutoMapperProfile));
+
+            #endregion
+
+            return services;
+        }
+    }
+}
 
